e3a4a674828b0bb5d29bdf0b1f665e064e786252
Sofiane Delloue <112469623+s8fe@users.noreply.github.com>
2023-06-08 22:07:17 +0200

    Update main.js

--- reel/main.js
+++ reel/main.js
@@ -4,16 +4,42 @@ const videoIds = ["12345678", "23456789", "34567890", "45678901", "56789012"];
 // Randomize the order of the video IDs
 videoIds.sort(() => Math.random() - 0.5); 
 
+const players = [];
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        const player = players.find(p => p.element === entry.target);
+        if (entry.isIntersecting) {
+            player.instance.play();
+        } else {
+            player.instance.pause();
+        }
+    });
+}, {
+    threshold: 0.5 // Adjust this value to decide when exactly to start and stop video play. This denotes the percentage of visibility of the video player in the viewport
+});
+
 videoIds.forEach(videoId => {
     const options = {
-        id: videoId, // The video ID
+        id: videoId,
+        width: window.innerWidth,
+        height: window.innerHeight,
         autoplay: false,
+        responsive: true,
     };
-    
+
     const playerContainer = document.createElement("div");
     document.getElementById("container").appendChild(playerContainer);
-    
+
     const player = new Vimeo.Player(playerContainer, options);
-    
+
     player.setVolume(0);
+
+    // We're keeping track of the player instances and their respective HTML elements here
+    players.push({
+        instance: player,
+        element: playerContainer,
+    });
+
+    observer.observe(playerContainer);
 });
