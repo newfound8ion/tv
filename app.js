406d3fce2f1db0ee2182cf806e71b67eb3536218
Sofiane Delloue <112469623+s8fe@users.noreply.github.com>
2023-06-08 04:09:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Fetch the video IDs from the JSON file
 async function fetchVideoIds() {
-    let response = await fetch('videos.json');
-    let data = await response.json();
-    return data;
+  let response = await fetch('videos.json');
+  let data = await response.json();
+  return data;
 }
 
 // Shuffle the video IDs array
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 // Create YouTube player
@@ -28,81 +28,78 @@ let currentIndex = 0;
 let isMuted = true;
 
 function onYouTubeIframeAPIReady() {
-    fetchVideoIds().then(response => {
-        videoIds = response;
-        shuffledVideoIds = shuffleArray(videoIds);
-        createPlayer();
-    });
+  fetchVideoIds().then((response) => {
+    videoIds = response;
+    shuffledVideoIds = shuffleArray(videoIds);
+    createPlayer();
+  });
 }
 
 function createPlayer() {
-    player = new YT.Player('player', {
-        height: '75vh',
-        width: '75vw',
-        playerVars: {
-            autoplay: 1,
-            controls: 0,
-            disablekb: 1,
-            rel: 0,
-            modestbranding: 1,
-            showinfo: 0,
-            playsinline: 1,
-            mute: 1 // Initially muted
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
+  player = new YT.Player('player', {
+    height: '75vh',
+    width: '75vw',
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      disablekb: 1,
+      rel: 0,
+      modestbranding: 1,
+      showinfo: 0,
+      playsinline: 1,
+      mute: isMuted ? 1 : 0,
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
 }
 
 function onPlayerReady(event) {
-    event.target.playVideo();
-    createUnmuteButton(); // Create the unmute button dynamically
-    setInterval(updateProgressBar, 200);
+  event.target.playVideo();
 }
 
 function onPlayerStateChange(event) {
-    if (event.data === YT.PlayerState.ENDED) {
-        playNextVideo();
-    }
+  if (event.data === YT.PlayerState.PLAYING && isMuted) {
+    player.mute();
+    isMuted = true;
+    updateAudioButton();
+  }
+
+  if (event.data === YT.PlayerState.ENDED) {
+    playNextVideo();
+  }
 }
 
 function playNextVideo() {
-    currentIndex++;
-    if (currentIndex >= shuffledVideoIds.length) {
-        currentIndex = 0;
-        shuffledVideoIds = shuffleArray(videoIds);
-    }
-    player.loadVideoById(shuffledVideoIds[currentIndex].id);
+  currentIndex++;
+  if (currentIndex >= shuffledVideoIds.length) {
+    currentIndex = 0;
+    shuffledVideoIds = shuffleArray(videoIds);
+  }
+  player.loadVideoById(shuffledVideoIds[currentIndex].id);
 }
 
-function unmutePlayer() {
+function toggleMute() {
+  if (isMuted) {
     player.unMute();
-    isMuted = false;
-    removeUnmuteButton(); // Remove the unmute button after it's clicked
+  } else {
+    player.mute();
+  }
+  isMuted = !isMuted;
+  updateAudioButton();
 }
 
-function createUnmuteButton() {
-    const container = document.getElementById('player-container');
-    const unmuteButton = document.createElement('button');
-    unmuteButton.innerText = 'Unmute';
-    unmuteButton.addEventListener('click', unmutePlayer);
-    container.appendChild(unmuteButton);
-}
-
-function removeUnmuteButton() {
-    const unmuteButton = document.querySelector('#player-container button');
-    if (unmuteButton) {
-        unmuteButton.remove();
-    }
-}
-
-function updateProgressBar() {
-    let playerProgress = (player.getCurrentTime() / player.getDuration()) * 100;
-    let progressBar = document.getElementById('progress-bar');
-    progressBar.style.width = playerProgress + '%';
+function updateAudioButton() {
+  const audioButton = document.getElementById('audio');
+  if (isMuted) {
+    audioButton.classList.add('muted');
+  } else {
+    audioButton.classList.remove('muted');
+  }
 }
 
 document.getElementById('logo').addEventListener('click', playNextVideo);
 document.getElementById('next').addEventListener('click', playNextVideo);
+document.getElementById('audio').addEventListener('click', toggleMute);
