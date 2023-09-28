946a504e1b2ae2d4e51da4f3909008904bac9a34
Sofiane Delloue <112469623+s8fe@users.noreply.github.com>
2023-09-28 21:35:55 +0200

    Update player.js

--- player.js
+++ player.js
@@ -6,13 +6,22 @@
     function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
             // Configuration options go here
+            videoId: 'your_video_id', // replace with your video id
+            events: {
+                'onReady': onPlayerReady,
+            }
         });
+    }
+
+    function onPlayerReady(event) {
+        event.target.mute(); // Start with muted audio
     }
 
     window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
 
     window.playerModule = {
         play: function() {
+            player.unMute(); // Ensure video is unmuted
             player.playVideo();
         },
         pause: function() {
