e15a1576efbfef349174d044f937f60599e94075
Sofiane Delloue <112469623+s8fe@users.noreply.github.com>
2023-09-28 21:32:26 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,15 +1,25 @@
 // ui.js
 (function(window) {
     var progressBar = document.getElementById('progress-bar');
+    var logo = document.getElementById('logo');
 
     function updateProgressBar(value) {
         progressBar.style.width = value + '%';
     }
 
-    // Add other UI related functions here
+    function doubleLogoWidth() {
+        var currentWidth = logo.offsetWidth;
+        logo.style.width = (currentWidth * 2) + 'px';
+    }
+
+    // Execute the function on window load or another event as needed
+    window.onload = function() {
+        doubleLogoWidth();
+    };
 
     window.uiModule = {
         updateProgressBar: updateProgressBar,
-        // Other UI related functions
+        doubleLogoWidth: doubleLogoWidth // Expose the function if needed elsewhere
+        // Add other UI related functions as needed
     };
 })(window);
